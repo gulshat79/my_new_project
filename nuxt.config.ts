fc0d6528918a2bf8a3f61151b329fd4a51f326d8
gulshat79 <gulshatsapayeva47@gmail.com>
2026-05-16 22:04:41 +0500

    PGISMSU-52 Объединение дублирующихся CSS-правил и исправление конфликта min-height для textarea

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,65 @@
 import { Preset } from './presets/preset'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['primeicons/primeicons.css', '~/assets/css/index.scss', '~/assets/css/main.css'],
-    modules: [
-        '@pinia/nuxt',
-        '@primevue/nuxt-module',
-        '@nuxt/test-utils/module',
-        '@element-plus/nuxt',
-        '@nuxtjs/i18n',
-        '@vueuse/nuxt'
-    ],
-    primevue: {
-        components: {
-            prefix: 'P'
-        },
-        options: {
-            theme: {
-                preset: Preset,
-                options: {
-                    darkModeSelector: '.p-dark'
-                }
-            }
+  ssr: false,
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preload',
+          as: 'font',
+          type: 'font/ttf',
+          href: '/fonts/Inter-VariableFont_opsz,wght.ttf',
+          crossorigin: 'anonymous'
         }
-    },
-    vite: {
-        plugins: []
-    },
-    devServer: {
-        host: '0.0.0.0',
-        port: 3340
-    },
-    runtimeConfig: {
-        public: {
-            extendApi: process.env.NUXT_PUBLIC_EXTEND_API
-        }
-    },
-    i18n: {
-        defaultLocale: 'ru',
-        strategy: 'no_prefix',
-        locales: ['ru', 'kg']
+      ]
     }
+  },
+  css: ['primeicons/primeicons.css', '~/assets/css/index.scss', '~/assets/css/main.css'],
+  devServer: {
+    host: '0.0.0.0',
+    port: 3340
+  },
+  runtimeConfig: {
+    public: {
+      extendApi: process.env.NUXT_PUBLIC_EXTEND_API
+    }
+  },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: false },
+  imports: {
+    dirs: ['composables/**']
+  },
+  modules: [
+    '@pinia/nuxt',
+    '@primevue/nuxt-module',
+    '@element-plus/nuxt',
+    '@nuxtjs/i18n',
+    '@vueuse/nuxt'
+  ],
+  primevue: {
+    components: {
+      prefix: 'P'
+    },
+    options: {
+      theme: {
+        preset: Preset,
+        options: {
+          darkModeSelector: '.p-dark'
+        }
+      }
+    }
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  i18n: {
+    defaultLocale: 'ru',
+    strategy: 'no_prefix',
+    locales: ['ru', 'kg'],
+    vueI18n: './i18n.config.ts',
+    plugins: [tailwindcss()]
+  }
 })
